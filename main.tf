d21d224fe96cdaa1e5eb9c3e5ba98e6ac6c81fe8
NBarabanschikov <NBarabanschikov@stream.ru>
2022-09-03 15:31:23 +0300

    create cluster

--- main.tf
+++ main.tf
@@ -13,3 +13,52 @@ provider "yandex" {
   folder_id = "b1gvsquk6jvkrc1a16m3"
   zone      = "ru-central1-b"
 }
+
+
+
+resource "yandex_kubernetes_cluster" "study-cluster" {
+ network_id = yandex_vpc_network.study.id
+ master {
+   zonal {
+     zone      = yandex_vpc_subnet.ru-central1-b.zone
+     subnet_id = yandex_vpc_subnet.study-subnet.id
+   }
+ }
+ service_account_id      = yandex_iam_service_account.study-sa.id
+ node_service_account_id = yandex_iam_service_account.study-node.id
+   depends_on = [
+     yandex_resourcemanager_folder_iam_binding.editor,
+     yandex_resourcemanager_folder_iam_binding.images-puller
+   ]
+}
+
+resource "yandex_vpc_network" "study" { name = "study" }
+
+resource "yandex_vpc_subnet" "study-subnet" {
+ v4_cidr_blocks = ["10.112.0.0/16"]
+ zone           = "ru-central1-b"
+ network_id     = yandex_vpc_network.study.id
+}
+
+resource "yandex_iam_service_account" "study-sa" {
+ name        = "study-sa"
+ description = "service account for cluster"
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "editor" {
+ # Сервисному аккаунту назначается роль "editor".
+ folder_id = "b1gvsquk6jvkrc1a16m3"
+ role      = "editor"
+ members   = [
+   "serviceAccount:${yandex_iam_service_account.study-sa.id}"
+ ]
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "images-puller" {
+ # Сервисному аккаунту назначается роль "container-registry.images.puller".
+ folder_id = "b1gvsquk6jvkrc1a16m3"
+ role      = "container-registry.images.puller"
+ members   = [
+   "serviceAccount:${yandex_iam_service_account.study-sa.id}"
+ ]
+}
