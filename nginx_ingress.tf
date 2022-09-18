46f3aa772b4e1b291337de25f45ff7682cfe18de
NBarabanschikov <NBarabanschikov@stream.ru>
2022-09-18 20:43:12 +0300

    add res helm example

--- nginx_ingress.tf
+++ nginx_ingress.tf
@@ -3,3 +3,16 @@ provider "helm" {
     config_path = "~/.kube/config"
   }
 }
+
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
